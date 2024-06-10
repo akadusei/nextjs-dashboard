190cdc1ef89ff2ffd2ff8b6576ee9ac91dd19aac
akadusei <attakusiadusei@gmail.com>
2024-06-10 19:02:40 +0000

    Opt out of caching with `noStore()`

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,5 @@
 import { sql } from '@vercel/postgres';
-// import { unstable_noStore as noStore } from 'next/cache'
+import { unstable_noStore as noStore } from 'next/cache'
 import {
   CustomerField,
   CustomersTableType,
@@ -14,7 +14,7 @@ import { formatCurrency } from './utils';
 export const fetchRevenue = async () => {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  // noStore()
+  noStore()
 
   try {
     // Artificially delay a response for demo purposes.
@@ -35,7 +35,7 @@ export const fetchRevenue = async () => {
 }
 
 export const fetchLatestInvoices = async () => {
-  // noStore()
+  noStore()
 
   // console.log('Fetching invoices...');
   // await new Promise((resolve) => setTimeout(resolve, 10000));
@@ -60,7 +60,7 @@ export const fetchLatestInvoices = async () => {
 }
 
 export const fetchCardData = async () => {
-  // noStore()
+  noStore()
 
   try {
     // You can probably combine these into a single SQL query
@@ -101,7 +101,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
-  // noStore()
+  noStore()
 
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
@@ -135,7 +135,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  // noStore()
+  noStore()
 
   try {
     const count = await sql`SELECT COUNT(*)
@@ -158,7 +158,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  // noStore()
+  noStore()
 
   try {
     const data = await sql<InvoiceForm>`
@@ -185,7 +185,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  // noStore()
+  noStore()
 
   try {
     const data = await sql<CustomerField>`
@@ -205,7 +205,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  // noStore()
+  noStore()
 
   try {
     const data = await sql<CustomersTableType>`
@@ -240,7 +240,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  // noStore()
+  noStore()
 
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
