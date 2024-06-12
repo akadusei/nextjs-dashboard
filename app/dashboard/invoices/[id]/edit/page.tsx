3a83026ccdb8fd0fda70487ae09745162d67189f
akadusei <attakusiadusei@gmail.com>
2024-06-12 17:17:57 +0000

    Add error handlers

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
+import { notFound } from 'next/navigation'
 
 export default async function Page({ params }) {
   const id = params.id
@@ -18,6 +19,8 @@ export default async function Page({ params }) {
     fetchCustomers(),
     fetchInvoiceById(id)
   ])
+
+  if (!invoice) notFound()
 
   return (
     <main>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,10 +26,14 @@ export const createInvoice = async (formData: FormData) => {
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+  } catch (error) {
+    return { message: 'Database Error: Failed to create invoice' }
+  }
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
@@ -44,11 +48,15 @@ export const updateInvoice = async (id: string, formData: FormData) => {
 
   const amountInCents = amount * 100
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `
+  } catch (error) {
+    return { message: 'Database Error: Failed to update invoice' }
+  }
 
   revalidatePath('/dashboard/invoices')
   revalidatePath(`/dashboard/invoices/${id}/edit`)
@@ -56,6 +64,13 @@ export const updateInvoice = async (id: string, formData: FormData) => {
 }
 
 export const deleteInvoice = async (id: string) => {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  throw new Error('Failed to delete invoice')
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted invoice' }
+  } catch (error) {
+    return { message: 'Database Error: Failed to delete invoice' }
+  }
 }
